d5f066d371812aa7f10c52b6bb0bee8a9ef1064a
mevershed <matthew@evershed.ca>
2017-01-17 14:47:28 -0500

    excercise 5

--- excercise5.rb
+++ excercise5.rb
@@ -1,5 +1,8 @@
-puts "What is the tempature in Fahrenheit?"
-F = gets
-def
-  
+def temp_celcius
+  puts "What is the tempature in Fahrenheit?"
+  temp_fahr = gets.to_i
+  temp_celc = (temp_fahr - 32) * 5 / 9
+p "The tempature in Celcius is #{temp_celc}"
 end
+
+temp_celcius
