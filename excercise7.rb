768d66dfdd9032cb549e44bc4fe2200f00a4d6e2
mevershed <matthew@evershed.ca>
2017-01-17 16:54:42 -0500

    step 1- 5

---
+++ excercise7.rb
@@ -0,0 +1,22 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+students.each do |cohort, size|
+  puts "#{cohort} : #{size}"
+end
+
+students[:cohort4] = 43
+
+students.each do |cohort, size|
+  puts "#{cohort} : #{size}"
+end
+
+students.each do |cohort, size|
+  puts "#{cohort} : #{size*1.05}"
+end
+
+students.each do |cohort, size|
+  puts "#{cohort} : #{size}"
+end
