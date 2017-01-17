7803355bd49d33edcd382ed9c48a0a5a4069ec14
mevershed <matthew@evershed.ca>
2017-01-17 15:52:45 -0500

    wasd

--- excercise6.rb
+++ excercise6.rb
@@ -1,5 +1,20 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 grocery_list.each do |value|
-  puts value
+  puts "* #{value}"
+end
+grocery_list << "rice"
+
+grocery_list.each do |value|
+  puts "* #{value}"
+
+end
+p grocery_list.length
+
+
+
+if grocery_list.include?("banana")
+  puts "You don't need to pick up bananas today"
+else
+  puts "You need to pick up bananas"
 end
