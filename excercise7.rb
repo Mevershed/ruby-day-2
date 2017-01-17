c8831c47fe7b83a795ba8658c7d94eb3e4fb9f21
mevershed <matthew@evershed.ca>
2017-01-17 17:18:54 -0500

    blah

--- excercise7.rb
+++ excercise7.rb
@@ -10,12 +10,18 @@ end
 students[:cohort4] = 43
 
 students.each do |cohort, size|
-  puts "#{cohort} : #{size}"
+  puts "#{cohort}"
 end
 
 students.each do |cohort, size|
   puts "#{cohort} : #{size*1.05}"
 end
+
+students.each do |cohort, size|
+  puts "#{cohort} : #{size}"
+end
+
+students.delete(2)
 
 students.each do |cohort, size|
   puts "#{cohort} : #{size}"
